00952c0a97ff4016857c79063f31844a8896d4b8
Ubuntu <ubuntu@sisp.idmkr.io>
2018-12-12 11:27:09 +0000

    ..

--- app/Http/Requests/StoreQuestion.php
+++ app/Http/Requests/StoreQuestion.php
@@ -26,7 +26,7 @@ class StoreQuestion extends FormRequest
     {
         return [
             'title' => 'required|max:255',
-            'description' => 'required|max:655',
+            'description' => 'required',
             'category' => 'required'
         ];
     }
