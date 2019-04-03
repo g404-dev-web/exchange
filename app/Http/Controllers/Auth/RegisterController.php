aaa80476daa9d01d10efb9139401390183ff500e
Hans Vanpée <hans@vanpee.be>
2019-04-03 14:16:04 +0200

    Correct user form validator

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,6 +69,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255|unique:users',
+            'fabric_ids' => 'required',
             'fabric_ids.*' => 'required|integer',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
